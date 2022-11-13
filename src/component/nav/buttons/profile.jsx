ca4522378731f416b41655fa8c0c4e4942fb056f
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2022-11-13 15:51:00 +0900

    merging conflicts

--- src/component/nav/buttons/profile.jsx
+++ src/component/nav/buttons/profile.jsx
@@ -4,14 +4,22 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../../../context/context";
 
 const ProfileButton = () => {
-    const { profilePic } = useContext(UserContext);
+  const { userId, profilePic } = useContext(UserContext);
   return (
-    <Link to="/profile">
-    <Button onClick={() => {}} style={{ width: 100, height: 70, color: 'black' }}>
-      <div style={{ borderRadius: "50%", width: "35px", height: "35px" }}>
-        <img src={profilePic} alt="profile" key={Date.now()} style={{ borderRadius: "50%", width: "35px", height: "35px" }}/>
-      </div>
-    </Button>
+    <Link to={`/profile?id=${userId}`}>
+      <Button
+        onClick={() => {}}
+        style={{ width: 100, height: 70, color: "black" }}
+      >
+        <div style={{ borderRadius: "50%", width: "35px", height: "35px" }}>
+          <img
+            src={profilePic}
+            alt="profile"
+            key={Date.now()}
+            style={{ borderRadius: "50%", width: "35px", height: "35px" }}
+          />
+        </div>
+      </Button>
     </Link>
   );
 };
