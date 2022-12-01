4f7bb99efce9c3fc5a007991c9ef1a288594be83
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2022-12-01 20:46:21 +0900

    add dns prefix

--- src/utils/prefix.js
+++ src/utils/prefix.js
@@ -1,2 +1,4 @@
-export const API = "http://3.39.56.248:8080";
+// export const API = "http://3.39.56.248:8080";
+export const API =
+  "https://ec2-3-39-56-248.ap-northeast-2.compute.amazonaws.com:8080/";
 // export const API = "http://localhost:8080";
