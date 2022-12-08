4019f11c0a54b7f28a837a5cd0444a3363b57781
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2022-12-08 11:56:19 +0900

    dockerized deployment

--- src/api/article/delete-article.js
+++ src/api/article/delete-article.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
 import { API } from "../../utils/prefix";
+import { deleteMultipleMedia } from "../media/delete-media";
 
 export const deleteArticle = async (articleId) => {
+  await deleteMultipleMedia(articleId);
   const request = `${API}/article?articleId=${articleId}`;
   const response = await Axios.delete(request);
   return response.data;
