5231799085ffe320adf241b24aecee14bafe25a3
Yany Choi <dydghks0818@naver.com>
2023-04-01 19:25:38 +0900

    Update prefix.js

--- src/utils/prefix.js
+++ src/utils/prefix.js
@@ -1,3 +1,4 @@
 // export const API = "http://3.39.56.248:8080";
-export const API = "https://api.yanychoi.site";
-// export const API = "http://localhost:8080";
+// export const API = "https://api.yanychoi.site";
+export const API = "https://instapicapi.yanychoi.site";
+// export const API = "http://192.168.219.120:8080";
